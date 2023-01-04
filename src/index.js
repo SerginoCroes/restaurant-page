c96c047672a6e5118a2845ab1ec3f33631ddf0df
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-01-04 14:21:07 +0100

    content to javascript, more styling, started menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('meh');
+import {drawhome} from "./homepage";
+
+const content = document.querySelector('#content');
+
+let mainContent = document.createElement('div');
+mainContent.classList.add('main');
+
+content.appendChild(drawhome().header);
+content.appendChild(mainContent);
+content.appendChild(drawhome().footer);
+
+console.log(drawhome().header);
+
+//drawhome();
